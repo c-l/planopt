e2af061b53ea34dbdd1f5f1e99d292b8d18aa4f0
Christopher Lin <c.l@berkeley.edu>
2015-07-05 17:34:06 -0700

    added sqp method which takes in cvxpy objective and constraints instead of matrixes

---
+++ problem_cvx.py
@@ -0,0 +1,125 @@
+from sqp_cvx import SQP
+import numpy as np
+import cvxpy as cvx
+import ipdb
+
+class Problem(object):
+    def __init__(self):
+        zerofunc = lambda x: np.matrix(0)
+        neginffunc = lambda x: np.matrix(-1e5)
+
+        n = 2
+
+        self.f = zerofunc
+        self.g = neginffunc
+        self.h = zerofunc
+        self.objective = 0
+        self.constraints = []
+
+    def problem0(self):
+        self.name = 'problem0'
+        self.x0 = np.matrix('1;1', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: np.matrix(x[0]**2+x[1]**2)
+        self.g = lambda x: np.matrix(3 - x[0] - x[1])
+        self.xtrue = np.matrix('1.5;1.5', dtype='float64')
+
+    def problem1(self):
+        self.name = 'problem1'
+        self.x0 = np.matrix('-2;1', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: (x[1]-x[0]**2)**2 + (1-x[0])**2
+        self.g = lambda x: np.matrix(-1.5 - x[1], dtype='float64')
+        self.xtrue = np.matrix('1;1', dtype='float64')
+
+    def problem2(self):
+        self.name = 'problem2'
+        self.x0 = np.matrix('10;1', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: x[1] + 1e-5 + (x[1]-x[0])**2
+        self.g = lambda x: -x[1]
+        self.xtrue = np.matrix('0;0', dtype='float64')
+
+    def problem3(self):
+        self.name = 'problem3'
+        self.x0 = np.matrix('10;1', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: (1-x[0])**2
+        self.h = lambda x: 10*(x[1]-x[0]**2)
+        self.xtrue = np.matrix('1;1', dtype='float64')
+
+    def problem4(self):
+        self.name = 'problem4'
+        self.x0 = np.matrix('2;2', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: np.log(1+x[0]**2)-x[1]
+        self.h = lambda x: (1+x[0]**2)**2+x[1]**2-4
+        self.xtrue = np.matrix('0;{0}'.format(np.sqrt(3)))
+
+    def problem5(self):
+        self.name = 'problem5'
+        angles = np.transpose(np.matrix(range(1,7)) * 2*np.pi/6)
+        self.x0 = np.matrix('0;0', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+
+        self.A_ineq = np.hstack((np.cos(angles),np.sin(angles)))
+        self.b_ineq = np.ones(angles.shape)
+        self.q = -np.matrix([np.cos(np.pi/6), np.sin(np.pi/6)])
+        self.constraints = [self.A_ineq * self.x <= self.b_ineq]
+        self.objective = self.q*self.x
+        self.xtrue = np.transpose(np.matrix([1, np.tan(np.pi/6)]))
+
+    def problem6(self):
+        self.name = 'problem6'
+        angles = np.transpose(np.matrix(range(1,7)) * 2*np.pi/6)
+        self.x0 = np.matrix('0;0', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.Q = .1*np.identity(2)
+        self.q = -np.matrix([np.cos(np.pi/6), np.sin(np.pi/6)])
+        A_ineq = np.hstack((np.cos(angles),np.sin(angles)))
+        b_ineq = np.ones(angles.shape)
+        self.g = lambda x: .01*(A_ineq*x-b_ineq)
+        # self.constraints = [A_ineq * self.x <= b_ineq]
+        self.objective = 0.5 * cvx.quad_form(self.x, self.Q) + self.q*self.x
+        self.xtrue = np.transpose(np.matrix([1, np.tan(np.pi/6)]))
+
+    def problem7(self):
+        self.name = 'problem7'
+        self.x0 = np.matrix('0;0', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.f = lambda x: x[0]**4 + x[1]**4
+        self.g = lambda x: 3 - x[0] - x[1]
+        self.h = lambda x: x[0] - 2*x[1]
+        self.xtrue = np.matrix('2;1', dtype='float64')
+
+    def problem8(self):
+        self.name = 'problem8'
+        self.x0 = np.matrix('5;5', dtype='float64')
+        self.x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        self.g = lambda x: np.vstack((\
+            x[0]**2 + x[1]**2 - 4, \
+            -((x[0]-1)**2 +(x[1]**2-1)**2 - 0.25), \
+            -((x[0]+1)**2 +(x[1]**2-1)**2 - 0.25), \
+            -((x[0])**2 + 7*(x[1]+1-x[0]**2/2)**2 - 0.8)))
+
+        self.Q = np.identity(2)
+        self.objective = 0.5 * cvx.quad_form(self.x, self.Q)
+        self.xtrue = np.matrix('0;0', dtype='float64')
+
+    def test(self):
+        sqp = SQP()
+        sqp.min_approx_improve = 1e-8
+        sqp.min_trust_box_size = 1e-5
+        # x = cvx.Variable(self.x0.shape[0], self.x0.shape[1])
+        x, success = sqp.penalty_sqp(self.x, self.x0, self.objective, self.constraints, self.f, self.g, self.h)
+        if success:
+            print ('{0} succeeded'.format(self.name))
+        else:
+            print ('{0} failed'.format(self.name))
+        if np.allclose(x.value, self.xtrue, atol=1e-3):
+            print ('x = {1} and xtrue = {2} are close'.format(self.name, x, self.xtrue))
+        else:
+            print ('x = {1} and xtrue = {2} are not close'.format(self.name, x, self.xtrue))
+
+
+
--- sqp_cvx.py
+++ sqp_cvx.py
@@ -5,12 +5,16 @@ import scipy.misc as sci
 import ipdb
 
 class SQP(object):
+    """
+    f is the function that needs to be approximately in the cost
+    g is the inequality constraint
+    h is the equality constraint
+    """
+
     def __init__(self):
         self.improve_ratio_threshold = .25
         self.min_trust_box_size = 1e-4
-        # self.min_trust_box_size = 1e-5
         self.min_approx_improve = 1e-4
-        # self.min_approx_improve = 1e-8
         self.max_iter = 50
         self.trust_shrink_ratio = .1
         self.trust_expand_ratio = 1.5
@@ -94,59 +98,56 @@ class SQP(object):
         a = A*x + b
         return np.sum(np.exp(a));
 
-    @staticmethod
-    def hinge(x):
-        return np.sum(np.maximum(x, np.zeros(x.shape)))
+    def penalty_sqp(self, x, x0, objective, constraints, f, g, h):
+        # trust_box_size = self.initial_trust_box_size
+        # penalty_coeff = self.initial_penalty_coeff
+        trust_box_size = cvx.Parameter(sign="positive", value = self.initial_trust_box_size)
+        penalty_coeff = cvx.Parameter(sign="positive", value = self.initial_penalty_coeff)
 
-    @staticmethod
-    def calc_merit(x, Q, q, f, g, h, penalty_coeff):
-        return f(x) + q*x + 0.5*np.transpose(x)*(Q*x) + penalty_coeff*(SQP.hinge(g(x)) + linalg.norm(h(x),1))
-
-    def penalty_sqp(self, x0, Q, q, f, A_ineq, b_ineq, A_eq, b_eq, g, h):
-        trust_box_size = self.initial_trust_box_size
-        penalty_coeff = self.initial_penalty_coeff
-        # penalty_coeff = 100
-
-        x, success = SQP.find_closest_feasible_point(x0, A_ineq, b_ineq, A_eq, b_eq)
+        x, success = SQP.find_closest_feasible_point(x, x0, constraints)
         if not success:
             return (x, success)
 
         for i in range(self.max_merit_coeff_increases):
-            x, trust_box_size, success = self.minimize_merit_function(x, Q, q, f, A_ineq, b_ineq, A_eq, b_eq, g, h, penalty_coeff, trust_box_size)
+            x, trust_box_size, success = self.minimize_merit_function(x, objective, constraints, f, g, h, penalty_coeff, trust_box_size)
             print '\n'
 
             constraints_satisfied = True
             if self.g_use_numerical:
-                gval = g(x)
-                gjac = SQP.numerical_jac(g,x)
+                gval = g(x.value)
+                gjac = SQP.numerical_jac(g,x.value)
             else:
-                gval, gjac = g(x)
-            if abs(h(x)) > self.cnt_tolerance:
+                gval, gjac = g(x.value)
+            # TODO: need to account for case where h is not numerical
+            if abs(h(x.value)) > self.cnt_tolerance:
                 constraints_satisfied = False
             elif any(gval > self.cnt_tolerance):
                 constraints_satisfied = False
 
             if not constraints_satisfied:
-                penalty_coeff = penalty_coeff*self.merit_coeff_increase_ratio
-                trust_box_size = self.initial_trust_box_size
+                penalty_coeff.value = penalty_coeff.value*self.merit_coeff_increase_ratio
+                trust_box_size.value = self.initial_trust_box_size
             else:
                 return (x, success)
             # ipdb.set_trace()
         return (x, False)
     
     @staticmethod
-    def find_closest_feasible_point(x0, A_ineq, b_ineq, A_eq, b_eq):
+    def find_closest_feasible_point(x, x0, constraints):
         success = True
+        
+        x.value = x0
+        feasible = True
+        for constraint in constraints:
+            if not np.all(constraint.value):
+                feasible = False
+                break
 
-        if any(A_ineq*x0 > b_ineq) or any(A_eq*x0 != b_eq):
-            print("initialization doesn't satisfy linear constraints. Finding the closest feasible point")
-
-            r, c = x0.shape
-            xp = cvx.Variable(r,c)
-            obj = cvx.norm(xp - x0,2)
+        if feasible:
+            return (x,success)
+        else:
+            obj = cvx.norm(x - x0,2)
             obj = cvx.Minimize(obj)
-            
-            constraints = [A_ineq*xp <= b_ineq, A_eq*xp == b_eq]
 
             prob = cvx.Problem(obj, constraints)
             # import ipdb; ipdb.set_trace()
@@ -158,67 +159,78 @@ class SQP(object):
             if prob.status != "optimal":
                 success = False
                 print("Couldn't find a point satisfying linear constraints")
-            return (xp.value, success)
-        return (x0, success)
+            return (x, success)
 
-    def minimize_merit_function(self, x, Q, q, f, A_ineq, b_ineq, A_eq, b_eq, g, h, penalty_coeff, trust_box_size):
-        dim_x = x.size
+    def update_fgh(self, x, f, g, h, fval, fgrad, fhess, gval, gjac, hval, hjac):
+        fval.value, fgrad.value, fhess.value, gval.value, gjac.value, hval.value, hjac.value = self.convexify_fgh(x, f, g, h)
+
+    def convexify_fgh(self, x, f, g, h):
+        fval = f(x.value)
+        fgrad, fhess = SQP.numerical_grad_hess(f,x.value)
+        
+        if self.g_use_numerical:
+            gval = g(x.value)
+            gjac = SQP.numerical_jac(g,x.value)
+        else:
+            gval, gjac = g(x.value)
+
+        hval = h(x.value)
+        hjac = SQP.numerical_jac(h,x.value)
+        return (fval, fgrad, fhess, gval, gjac, hval, hjac)
+
+    def minimize_merit_function(self, xp, objective, constraints_wo_trust_box, f, g, h, penalty_coeff, trust_box_size):
+        objective_wo_fhess = objective
 
         success = True
         sqp_iter = 1
 
+        # x is a cvxpy variable
+        rows, cols = xp.size
+        x = cvx.Parameter(rows, cols, value=xp.value)
+        
+        # computing convex approximations of f, g, h
+        fval, fgrad, fhess, gval, gjac, hval, hjac = self.convexify_fgh(x, f, g, h)
+
+        # setting up parameters for approximations of f,g and h
+        # setting up parameters allows cvxpy to avoid setting up the problem every iteration
+        fval = cvx.Parameter(fval.shape[0], fval.shape[1], value=fval)
+        fgrad = cvx.Parameter(fgrad.shape[0], fgrad.shape[1], value=fgrad)
+        fhess = cvx.Parameter(fhess.shape[0], fhess.shape[1], value=fhess)
+        gval = cvx.Parameter(gval.shape[0], gval.shape[1], value=gval)
+        gjac = cvx.Parameter(gjac.shape[0], gjac.shape[1], value=gjac)
+        hval = cvx.Parameter(hval.shape[0], hval.shape[1], value=hval)
+        hjac = cvx.Parameter(hjac.shape[0], hjac.shape[1], value=hjac)
+ 
+        # set up objective and constraints
+        penalty_objective = cvx.sum_entries(cvx.pos(gval + gjac*(xp-x)))
+        penalty_objective += cvx.norm(hval + hjac*(xp-x), 1)
+
+        objective_wo_fhess += fval
+        objective_wo_fhess += fgrad*(xp-x)
+        objective_wo_fhess += penalty_coeff * penalty_objective
+
+        objective = objective_wo_fhess
+        if np.any(fhess.value):
+            objective += cvx.quad_form(xp-x, fhess)
+            # obj += np.transpose(xp-x)*fhess*(xp-x)
+        # objective = cvx.Minimize(objective)
+
+        constraints = constraints_wo_trust_box + [cvx.norm(xp-x,2) <= trust_box_size]
+        prob = cvx.Problem(cvx.Minimize(objective), constraints)
+
+       
         while True:
-            # print("x: {0}".format(x))
             print("  sqp_iter: {0}".format(sqp_iter))
-
-            # get gradients anad hess info for f,g and h
-            #Use parameters for gradients and hess and trust box?
-            #sci.derivative(f, x0, dx, n)
-            fval = f(x)
-            fgrad, fhess = SQP.numerical_grad_hess(f,x)
-            if self.g_use_numerical:
-                gval = g(x)
-                gjac = SQP.numerical_jac(g,x)
-            else:
-                gval, gjac = g(x)
-            hval = h(x)
-            hjac = SQP.numerical_jac(h,x)
-
-            merit = SQP.calc_merit(x, Q, q, f, lambda x: gval, h, penalty_coeff)
+            merit = objective.value
 
             while True:
-                print("    trust region size: {0}".format(trust_box_size))
+                print("    trust region size: {0}".format(trust_box_size.value))
 
-                xp = cvx.Variable(dim_x)
-
-                #better name?
-                penalty_obj = cvx.sum_entries(cvx.pos(gval + gjac*(xp-x)))
-                penalty_obj += cvx.norm(hval + hjac*(xp-x), 1)
-
-                obj = q*xp
-                # ipdb.set_trace()
-                if np.any(Q):
-                    obj += 0.5 * cvx.quad_form(xp, Q)
-                obj += fval
-                obj += fgrad*(xp-x)
-                if np.any(fhess):
-                    obj += cvx.quad_form(xp-x, fhess)
-                    # obj += np.transpose(xp-x)*fhess*(xp-x)
-                obj += penalty_coeff * penalty_obj
-
-                obj = cvx.Minimize(obj)
-
-                constraints = [A_ineq*xp <= b_ineq, A_eq*xp == b_eq, cvx.norm(xp-x,2) <= trust_box_size]
-
-                xp.value = x
-                print "obj value: ", obj.value
-                print "merit: ", merit
-
-                if not np.isclose(obj.value, merit, atol=1e-3):
-                    ipdb.set_trace()
-
-                prob = cvx.Problem(obj, constraints)
                 try:
+                    objective = objective_wo_fhess
+                    if np.any(fhess.value):
+                        objective += cvx.quad_form(xp-x, fhess)
+                    prob = cvx.Problem(cvx.Minimize(objective), constraints)
                     prob.solve(verbose=False, solver='GUROBI')
                 except:
                     print ("solver error")
@@ -227,20 +239,28 @@ class SQP(object):
                     print 'problem status: ', prob.status
                     print('Failed to solve QP subproblem.')
                     success = False
-                    return (x, trust_box_size, success)
-
-                # print("xp value: {0}".format(xp.value))
+                    return (xp, trust_box_size, success)
 
                 model_merit = prob.value
 
-                print 'evaluating for xp'
-                # fix bug that f and h aren't recomputed
-                if self.g_use_numerical:
-                    gval2 = g(xp.value)
-                    # gjach = SQP.numerical_jac(g,xp.value)
-                else:
-                    gval2, gjac2 = g(xp.value)
-                new_merit = SQP.calc_merit(xp.value, Q, q, f, lambda x: gval2, h, penalty_coeff)
+                # saving current convexified approximations of f,g and h
+                fval_cur = fval.value
+                fgrad_cur = fgrad.value
+                fhess_cur = fhess.value
+
+                gval_cur = gval.value
+                gjac_cur = gjac.value
+
+                hval_cur = hval.value
+                hjac_cur = hjac.value
+
+                x_cur = x.value
+
+                self.update_fgh(xp, f, g, h, fval, fgrad, fhess, gval, gjac, hval, hjac)
+
+                x.value = xp.value
+                new_merit = objective.value
+
                 approx_merit_improve = merit - model_merit
                 exact_merit_improve = merit - new_merit
                 merit_improve_ratio = exact_merit_improve / approx_merit_improve
@@ -251,30 +271,42 @@ class SQP(object):
                     print("Approximate merit function got worse ({0})".format(approx_merit_improve))
                     print("Either convexification is wrong to zeroth order, or you're in numerical trouble.")
                     success = False
-                    return (x, trust_box_size, success)
+                    xp.value = x_cur
+                    return (xp, trust_box_size, success)
                 elif approx_merit_improve < self.min_approx_improve:
                     print("Converged: y tolerance")
-                    x = xp.value
                     #some sort of callback
-                    return (xp.value, trust_box_size, success)
+                    print "x: ", xp.value
+                    return (xp, trust_box_size, success)
                 elif (exact_merit_improve < 0) or (merit_improve_ratio < self.improve_ratio_threshold):
+                    # reset convex approximations of f,g and h to their original values
+                    fval.value = fval_cur
+                    fgrad.value = fgrad_cur
+                    fhess.value = fhess_cur
+                    gval.value = gval_cur
+                    gjac.value = gjac_cur
+                    hval.value = hval_cur
+                    hjac.value = hjac_cur
+
+                    x.value = x_cur
+
                     print("Shrinking trust region")
-                    trust_box_size = trust_box_size * self.trust_shrink_ratio
+                    trust_box_size.value = trust_box_size.value * self.trust_shrink_ratio
                 else:
                     print("Growing trust region")
-                    trust_box_size = trust_box_size * self.trust_expand_ratio
-                    x = xp.value
+                    trust_box_size.value = trust_box_size.value * self.trust_expand_ratio
+                    print "x: ", xp.value
                     break #from trust region loop
 
-                if trust_box_size < self.min_trust_box_size:
+                if trust_box_size.value < self.min_trust_box_size:
                     print("Converged: x tolerance")
-                    return (x, trust_box_size, success) 
+                    return (xp, trust_box_size, success) 
 
             sqp_iter = sqp_iter + 1
 
 
 def test_sqp():
-    from problem import Problem
+    from problem_cvx import Problem
     prob = Problem()
     prob.problem8()
     prob.test()
