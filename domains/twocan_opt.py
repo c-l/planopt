082a7b135538a3332cae25c7c7379561903aa4f9
Rohan Chitnis <ronuchit@yahoo.com>
2016-02-25 00:19:41 -0800

    re-add world state..for some reason fixes things

--- domains/twocan_opt.py
+++ domains/twocan_opt.py
@@ -30,7 +30,8 @@ class TwoCanOpt(object):
                                "goal2":ObjLoc("goal2", self.rows, self.cols, is_var=False, value=np.array([[3.5], [4.5], [0]]))})
 
         self.name = "twocan_world"
-        self.world_state = {}
+        self.world_state = {self.param_map["can1"]: self.param_map["can1initloc"],
+                            self.param_map["can2"]: self.param_map["can2initloc"]}
 
     def get_sampled_params(self):
         params_to_sample = []
