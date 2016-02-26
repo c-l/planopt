ac376d9f1114e9ae29eff4a12f72e94892a9ef04
Christopher Lin <c.l@berkeley.edu>
2016-02-26 14:38:50 -0800

    updated is_gp's formulation to more closely match in_manip's constraints, updated pick test to use small cans environment

--- src/tests/test_pr2_pick.py
+++ src/tests/test_pr2_pick.py
@@ -150,7 +150,55 @@ def test_pick_full_DOF():
     ll_prob.solve_at_priority(-1)
     ll_prob.solve_at_priority(1)
 
+def test_pick_small_cans_full_DOF():
+    env = pr2_small_cans_test_env()
+
+    # robot = PR2('pr2')
+    # active_body_parts = ['rightarm', 'rgripper', 'torso', 'base']
+
+    # active_bodyparts = ['rightarm', 'rgripper', 'base']
+    active_bodyparts = ['rightarm', 'rgripper']
+    robot = PR2('pr2', active_bodyparts)
+    robot.tuck_arms(env)
+
+    hl_plan = TestDomain(env)
+    pick_env = env.CloneSelf(1) # clones objects in the environment
+    robot.init_for_env(env)
+    robot.init_for_env(pick_env)
+
+    pose = robot.get_pose(env)
+    robot_body = robot.get_env_body(env)
+
+    hl_plan = HLPlan(env)
+    obj = Obj('object7')
+    K = robot.dofs
+    # obj_pose = np.array([[0.],[0.],[0.]])
+    # obj.set_pose(env, obj_pose)
+
+    # start = HLParam("pos", self.K, 1, is_var=False, value=robot.get_pose(env))
+    # pose = np.array([[-1.57265580e+00], [5.27875957e-01], [-1.40000000e+00], \
+    #     [-1.58244363e+00], [-1.64143184e+00], [-9.76938766e-02], [-5.78403045e-01], [0.0],[0.],[0.],[0.]])
+    # pose = np.array([[-1.57265580e+00], [5.27875957e-01], [-1.40000000e+00], \
+    #     [-1.58244363e+00], [-1.64143184e+00], [-9.76938766e-02], [-5.78403045e-01]])
+    pose = np.array([[-1.57265580e+00], [5.27875957e-01], [-1.40000000e+00], \
+        [-1.58244363e+00], [-1.64143184e+00], [-9.76938766e-02], [-5.78403045e-01], [.4]])
+    start = HLParam("start", K, 1, value=pose, is_var=False)
+    # start = HLParam("start", K, 1, value=pose, is_var=True)
+    end = HLParam("end", K, 1, value=pose)
+    # end = HLParam("end", K, 1, is_var=True, value=end_pose)
+    # obj_pos = HLParam("obj_pos", 3, 1, is_var=False, value=obj.get_pose(env))
+
+    pick = PR2Pick(0, hl_plan, pick_env, robot, start, end, obj)
+    # pick.preconditions[-1].error(pose)
+    hlas = [pick]
+    ll_prob = LLProb(hlas)
+    # ll_prob.solve()
+    # import ipdb; ipdb.set_trace()
+    ll_prob.solve_at_priority(-1)
+    ll_prob.solve_at_priority(1)
+
 if __name__ == "__main__":
     # test_pick_pr2_gradient()
     # test_pick()
-    test_pick_full_DOF()
+    # test_pick_full_DOF()
+    test_pick_small_cans_full_DOF()
